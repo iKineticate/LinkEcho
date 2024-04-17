cf17c95944cec71be7fd39ca946b4abe561698d8
Leen_Joan <1159361934@qq.com>
2024-04-17 11:25:28 +0800

    获取不同快捷方式的目标扩展名

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // #![windows_subsystem = "windows"]       // 隐藏 CMD 和 Powershel
+#[cfg(target_os = "windows")]
 
 use std::env;
 use std::path::{Path, PathBuf};
@@ -16,8 +17,6 @@ struct LinkInfo {
     link_target_dir: String,
     link_icon_location: String,
 }
-
-
 fn main() {
     // 获取管理员权限
 
@@ -100,15 +99,15 @@ fn get_link_info(path_buf: &PathBuf) -> (String, String, String, String, String,
     let link_name = path_buf.file_stem()      //  Option<&OsStr> -> OsStr -> Option<&str> -> &string
         .map_or_else(|| "unnamed_file".to_string()
         , |no_extension| no_extension.to_string_lossy().into_owned().trim().to_lowercase());
-
+    // 快捷方式实际路径
     let link_path = path_buf.to_string_lossy().into_owned();      //  PathBuf -> Cow<str> -> String ( to_string_lossy : 任何非 Unicode 序列都将替换为 �)
-
+    // 引用 parselnk 库
     let objlnk = Lnk::try_from(path_buf.as_path()).unwrap();
-
-    let link_working_dir = objlnk.working_dir().unwrap_or_else(|| PathBuf::new()).to_string_lossy().into_owned();     // Option<PathBuf> -> PathBuf -> Cow<str> ->  String ( to_string_lossy : 任何非 Unicode 序列都将替换为 �)
-
-    // 解决 parselnk 库不能获取 link_target_path 的问题
+    // 快捷方式目标目录
+    let mut link_working_dir = objlnk.working_dir().unwrap_or_else(|| PathBuf::new()).to_string_lossy().into_owned();     // Option<PathBuf> -> PathBuf -> Cow<str> ->  String ( to_string_lossy : 任何非 Unicode 序列都将替换为 �)
+    // 快捷方式目标路径
     let mut link_target_path = String::new();
+    // 解决 parselnk 库不能获取 link_target_path 的问题
     if let Some(have_string) = objlnk.link_info.local_base_path {
         link_target_path = have_string;
     } else if let Some(path_buf) = objlnk.relative_path() {
@@ -123,51 +122,63 @@ fn get_link_info(path_buf: &PathBuf) -> (String, String, String, String, String,
     } else {
         link_target_path = link_working_dir.clone();
     }
-
-    let link_working_dir = if link_working_dir.is_empty() {
-        Path::new(&link_target_path).parent().unwrap_or_else(|| Path::new("")).to_string_lossy().into_owned()    // Option<&OsStr> -> OsStr -> Cow<str> ->  String
-    } else {
-        link_working_dir
+    // 解决目标目录为空的情况
+    if link_working_dir.is_empty() {
+        link_working_dir = Path::new(&link_target_path).parent().unwrap_or_else(|| Path::new("")).to_string_lossy().into_owned()    // Option<&OsStr> -> OsStr -> Cow<str> ->  String
     };
-
+    // 快捷方式图标路径
     let link_icon_location = objlnk.string_data.icon_location.unwrap_or_else(|| PathBuf::new()).to_string_lossy().into_owned();     // Option<PathBuf> -> PathBuf -> Cow<str> ->  String ( to_string_lossy : 任何非 Unicode 序列都将替换为 �)
-    
+    // 快捷方式图标索引号
     let icon_index = objlnk.header.icon_index;      // u32，返回值不太对，比如ahk返回-108，它返回4294967188
-
-    let link_ext = if Path::new(&link_target_path).exists() {
-        Path::new(&link_target_path).extension()
-            .map_or_else(|| "".to_string()
-            , |os_str| os_str.to_string_lossy().into_owned().trim().to_lowercase())    // Option<&OsStr> -> OsStr -> Cow<str> ->  String
+    // 快捷方式目标扩展名
+    let mut link_target_ext = String::new();
+    if !Path::new(&link_target_path).is_file() {
+        link_target_ext = String::from("uwp|app");
     } else {
-        String::from("uwp/app")
+        let target_file_name = Path::new(&link_target_path).file_name()
+            .map_or_else(|| String::new(), 
+            |name| name.to_string_lossy().into_owned());
+        let target_extension = Path::new(&link_target_path).extension();    // Option<OsStr>
+        match &*target_file_name {
+            "schtasks.exe"   => {link_target_ext = String::from("schtasks")}, // 任务计划程序
+            "explorer.exe"   => {link_target_ext = String::from("explorer")}, // 资源管理器
+            "cmd.exe"        => {link_target_ext = String::from("cmd")},      // 命令提示符
+            "powershell.exe" => {link_target_ext = String::from("psh")},      // PowerShell
+            "wscript.exe"    => {link_target_ext = String::from("wscript")},  // WScript 对象
+            "mstsc.exe"      => {link_target_ext = String::from("mstsc")},    // 远程连接
+            "control.exe"    => {link_target_ext = String::from("control")},  // 控制面板
+            _ => {
+                match &target_extension {
+                    None => {},
+                    Some(_) if link_target_path.contains("WindowsSubsystemForAndroid") => {link_target_ext = String::from("app")},
+                    Some(os_str) => {link_target_ext = os_str.to_string_lossy().into_owned().to_lowercase()},
+                }
+            }
+        }
     };
 
-    (link_name, link_path, link_working_dir, link_target_path, link_icon_location, icon_index.to_string(), link_ext)
+    (link_name, link_path, link_working_dir, link_target_path, link_icon_location, icon_index.to_string(), link_target_ext)
 }
 
 fn find_link_files(directory: &Path, link_map: &mut HashMap<(String, String), LinkInfo>) {
     let directory_pattern = format!(r"{}\**\*.lnk", directory.to_string_lossy());
 
     for path_buf in glob(&directory_pattern).unwrap().filter_map(Result::ok) {
-        let (link_name, link_path, link_working_dir, link_target_path, link_icon_location, icon_index, link_ext) = get_link_info(&path_buf);
+        let (link_name, link_path, link_working_dir, link_target_path, link_icon_location, icon_index, link_target_ext) = get_link_info(&path_buf);
 
-        println!("名称：{}, 扩展名：{}", link_name, link_ext);
-        println!("{}", link_path);
-        println!("目标目录：{}", link_working_dir);
-        println!("目标位置：{}", link_target_path);
-        println!("图标位置：{}", link_icon_location);
-        println!("图标索引：{}", icon_index);
-        println!("");
+        println!("名称：{}, 扩展名：{}", link_name, link_target_ext);println!("{}", link_path);
+        println!("目标目录：{}", link_working_dir);println!("目标位置：{}", link_target_path);
+        println!("图标位置：{}", link_icon_location);println!("图标索引：{}", icon_index);println!("");
 
         // 排除重复项目
-        if link_map.contains_key(&(link_name.clone(), link_ext.clone())) {
+        if link_map.contains_key(&(link_name.clone(), link_target_ext.clone())) {
             continue;
         }
 
         // 添加至列表
 
-        // 最后添加至 HashMap，&str会影响生命周期，所以用String了
-        link_map.insert((link_name, link_ext), LinkInfo {
+        // 最后添加至 HashMap，&str会影响生命周期，所以用String
+        link_map.insert((link_name, link_target_ext), LinkInfo {
             link_path: link_path,
             link_target_path: link_target_path,
             link_target_dir: link_working_dir,
@@ -223,24 +234,24 @@ fn change_all_shortcuts_icons(link_map: &mut HashMap<(String, String), LinkInfo>
         };
         let icno_name_len = icno_name.len();
         // 遍历 HashMap
-        for (link_name, link_ext) in link_map.keys() {
+        for (link_name, link_target_ext) in link_map.keys() {
             // 跳过已匹配对象
-            if match_same_vec.contains(&(link_name, link_ext)) {
+            if match_same_vec.contains(&(link_name, link_target_ext)) {
                 continue;
             }
             // 匹配图标与lnk名称之间的包含关系
             match (link_name.len().cmp(&icno_name_len), link_name.cmp(&icno_name)) {
-                (Ordering::Equal, Ordering::Equal) => match_same_vec.push((link_name, link_ext)),
+                (Ordering::Equal, Ordering::Equal) => match_same_vec.push((link_name, link_target_ext)),
                 (Ordering::Greater, _)  if link_name.contains(&icno_name) => {},
                 (Ordering::Less, _)  if icno_name.contains(&*link_name) => {},
                 _ => continue,
             }
             // 跳过存在包含关系但已为使用图标情况
-            if link_map.get(&(link_name.to_string(), link_ext.to_string())).unwrap().link_icon_location == icon_path {
+            if link_map.get(&(link_name.to_string(), link_target_ext.to_string())).unwrap().link_icon_location == icon_path {
                 continue;
             };
             // 追加 PowerShell 命令
-            let link_path = &link_map.get(&(link_name.to_string(), link_ext.to_string())).unwrap().link_path;
+            let link_path = &link_map.get(&(link_name.to_string(), link_target_ext.to_string())).unwrap().link_path;
             command.push_str(&format!(
                 r#"
                 $shortcut = $shell.CreateShortcut("{link_path}")
